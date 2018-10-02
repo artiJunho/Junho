f6e34a7189d9df98fe5a52427b8fb1d161cff971
artiJunho <cartavia@junhoapp.com>
2018-10-01 22:43:29 -0600

    F - Init commit React Redux
    - Add React Native Navigation
    - Add Redux
    - EsLint
    - Update Readme

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+
+import reducer from './reducer';
+import RepoList from './RepoList';
+
+const client = axios.create({
+    baseURL: 'https://api.github.com',
+    responseType: 'json'
+});
+
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
 
 export default class App extends React.Component {
     render() {
         return (
-            <Container>
-                <Header />
-                <Content />
-                <Footer>
-                    <FooterTab>
-                        <Button>
-                            <Icon name="home" />
-                        </Button>
-                        <Button active>
-                            <Icon active name="camera" />
-                        </Button>
-                        <Button>
-                            <Icon name="person" />
-                        </Button>
-                    </FooterTab>
-                </Footer>
-            </Container>
+            // <Container>
+            //     <Header />
+            //     <Content />
+            //     <Footer>
+            //         <FooterTab>
+            //             <Button>
+            //                 <Icon name="home" />
+            //             </Button>
+            //             <Button active>
+            //                 <Icon active name="camera" />
+            //             </Button>
+            //             <Button>
+            //                 <Icon name="person" />
+            //             </Button>
+            //         </FooterTab>
+            //     </Footer>
+            // </Container>
+            <Provider store={store}>
+                <View style={styles.container}>
+                    <RepoList />
+                </View>
+            </Provider>
         );
     }
 }
@@ -42,4 +60,9 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: '500'
     },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: 50
+    }
 });
